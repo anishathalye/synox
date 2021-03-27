8e38b893ad38e40a02a6b496870d1ddd09e8f3f8
Anish Athalye <me@anishathalye.com>
2021-03-26 21:10:22 -0400

    Tweak interface

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,20 @@ use input_data_graph::InputDataGraph;
 pub use language::StringProgram;
 
 pub fn learn(
-    all_unpaired: &[Vec<String>],
+    unpaired: &[Vec<String>],
     examples: &[(Vec<String>, String)],
 ) -> Option<impl StringProgram> {
-    let graph = InputDataGraph::new(all_unpaired);
+    // NOTE we construct all_unpaired here rather than having the caller pass it in, so we can
+    // enforce the precondition of Dag::learn that the indices of the examples correspond to the
+    // indices used in constructing the graph (so all unpaired has to be examples concatenated with
+    // the unpaired, in that order)
+    let all_unpaired: Vec<_> = examples
+        .iter()
+        .map(|(row, _)| row)
+        .chain(unpaired.iter())
+        .cloned()
+        .collect();
+    let graph = InputDataGraph::new(&all_unpaired);
     let dag = Dag::learn(examples, &graph);
     dag.top_ranked_expression(&graph)
 }
